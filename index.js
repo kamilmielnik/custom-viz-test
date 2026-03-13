e0bbe87e37600f5e9d5e97544af087843b8b75cb
Kamil Mielnik <kamil@kamilmielnik.com>
2026-03-13 18:32:39 +0700

    Update title of threshold setting

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const { jsx: _jsx } = window.__METABASE_VIZ_API__.jsxRuntime;
       settings: {
         threshold: {
           id: "1",
-          title: "Threshold",
+          title: "Threshold updated",
           widget: "number",
           getDefault() {
             return 0;
